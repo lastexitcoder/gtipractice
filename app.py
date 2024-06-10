1b78e9c6018ab96ab734955c3098ed8615237421
Last Exit <lastexit1029@outlook.com>
2024-06-10 22:13:26 +0400

    multiplication updated

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 def addition(a, b, c):
     print(a + b + c)
+## addition code
 
-def multiplication(a, b):
-    return a * b
+def addition(a, b, c):
+    return a + b + c
+
+def multiplication(a, b, c):
+    print(a * b * c)
+    return a * b * c
